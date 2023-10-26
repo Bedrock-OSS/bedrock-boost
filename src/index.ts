acd65260903e001b8cfda38c2c45cc70893d1c66
stirante <brzozowski.s.piotr@gmail.com>
2023-10-26 12:29:26 +0200

    Add profiling dummy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Vec3 from "./vec3";
 import Timings from "./timings";
+import { addIdleDummy, clearIdleDummy } from "./profilingUtils";
 
 
-export { Vec3, Timings };
+export { Vec3, Timings, addIdleDummy, clearIdleDummy };
