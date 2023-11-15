992411a3e63d3ad18c74c2b30db54834e3ba14bb
stirante <brzozowski.s.piotr@gmail.com>
2023-11-15 09:57:07 +0100

    Add getDimension function to exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Vec3 from "./vec3";
 import Timings from "./timings";
 import { addIdleDummy, clearIdleDummy } from "./profilingUtils";
+import { getDimension } from "./cache";
 
 
-export { Vec3, Timings, addIdleDummy, clearIdleDummy };
+export { Vec3, Timings, addIdleDummy, clearIdleDummy, getDimension };
