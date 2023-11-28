9dab499368e45ec7498bd8df3ba56622d208f0e0
stirante <brzozowski.s.piotr@gmail.com>
2023-11-28 14:00:05 +0100

    Add ChatColor and Logger

---
+++ src/chatColor.ts
@@ -0,0 +1,156 @@
+/**
+ * ChatColor is a class for defining color codes.
+ */
+export default class ChatColor {
+    /**
+     * PREFIX is the section sign (§) used in Minecraft color codes.
+     */
+    private static readonly PREFIX = '§';
+
+    public static BLACK: ChatColor = new ChatColor('0');
+    public static DARK_BLUE: ChatColor = new ChatColor('1');
+    public static DARK_GREEN: ChatColor = new ChatColor('2');
+    public static DARK_AQUA: ChatColor = new ChatColor('3');
+    public static DARK_RED: ChatColor = new ChatColor('4');
+    public static DARK_PURPLE: ChatColor = new ChatColor('5');
+    public static GOLD: ChatColor = new ChatColor('6');
+    public static GRAY: ChatColor = new ChatColor('7');
+    public static DARK_GRAY: ChatColor = new ChatColor('8');
+    public static BLUE: ChatColor = new ChatColor('9');
+    public static GREEN: ChatColor = new ChatColor('a');
+    public static AQUA: ChatColor = new ChatColor('b');
+    public static RED: ChatColor = new ChatColor('c');
+    public static LIGHT_PURPLE: ChatColor = new ChatColor('d');
+    public static YELLOW: ChatColor = new ChatColor('e');
+    public static WHITE: ChatColor = new ChatColor('f');
+    public static MINECOIN_GOLD: ChatColor = new ChatColor('g');
+    public static MATERIAL_QUARTZ: ChatColor = new ChatColor('h');
+    public static MATERIAL_IRON: ChatColor = new ChatColor('i');
+    public static MATERIAL_NETHERITE: ChatColor = new ChatColor('j');
+    public static MATERIAL_REDSTONE: ChatColor = new ChatColor('m');
+    public static MATERIAL_COPPER: ChatColor = new ChatColor('n');
+    public static MATERIAL_GOLD: ChatColor = new ChatColor('p');
+    public static MATERIAL_EMERALD: ChatColor = new ChatColor('q');
+    public static MATERIAL_DIAMOND: ChatColor = new ChatColor('s');
+    public static MATERIAL_LAPIS: ChatColor = new ChatColor('t');
+    public static MATERIAL_AMETHYST: ChatColor = new ChatColor('u');
+    public static OBFUSCATED: ChatColor = new ChatColor('k');
+    public static BOLD: ChatColor = new ChatColor('l');
+    public static ITALIC: ChatColor = new ChatColor('o');
+    public static RESET: ChatColor = new ChatColor('r');
+
+    /**
+     * Different ChatColor instances for different tokens in JSON.
+     */
+    private static readonly OBJECT_BRACKETS_COLOR: ChatColor = ChatColor.YELLOW;
+    private static readonly ARRAY_BRACKETS_COLOR: ChatColor = ChatColor.AQUA;
+    private static readonly NUMBER_COLOR: ChatColor = ChatColor.DARK_AQUA;
+    private static readonly STRING_COLOR: ChatColor = ChatColor.DARK_GREEN;
+    private static readonly BOOLEAN_COLOR: ChatColor = ChatColor.GOLD;
+    private static readonly NULL_COLOR: ChatColor = ChatColor.GOLD;
+    private static readonly KEY_COLOR: ChatColor = ChatColor.GRAY;
+    private static readonly ESCAPE_COLOR: ChatColor = ChatColor.GOLD;
+    private static readonly FUNCTION_COLOR: ChatColor = ChatColor.GRAY;
+    private static readonly CYCLE_COLOR: ChatColor = ChatColor.DARK_RED;
+    private static readonly COMPACT_THRESHOLD: number = 60;
+
+    constructor(private code: string) {
+    }
+
+    /**
+     * Returns the string representation of the ChatColor instance,
+     * which is the PREFIX followed by the color code.
+     * @returns PREFIX + this.code
+     */
+    public toString() {
+        return ChatColor.PREFIX + this.code;
+    }
+
+    static stripColor(str: string) {
+        return str.replace(/§[0-9a-u]/g, '');
+    }
+    /**
+   * Transforms a value into a chat-friendly, colored JSON representation.
+   * @param value - The value to transform.
+   */
+    static prettyChatJSON(value: any): string {
+        return this.prettyChatJSONInternal(value);
+    }
+    static prettyChatJSONInternal(value: any, indentLevel: number = 0, knownElements: Set<any> = new Set()): string {
+        const indentSpace = ' '.repeat(indentLevel * 2);
+
+        if (value === null) return `${ChatColor.NULL_COLOR}null${ChatColor.RESET}`;
+        if (value === void 0) return `${ChatColor.NULL_COLOR}undefined${ChatColor.RESET}`;
+        if (typeof value === 'number') return `${ChatColor.NUMBER_COLOR}${value}${ChatColor.RESET}`;
+        if (typeof value === 'string') return `${ChatColor.STRING_COLOR}"${this.escapeString(value)}"${ChatColor.RESET}`;
+        if (typeof value === 'boolean') return `${ChatColor.BOOLEAN_COLOR}${value}${ChatColor.RESET}`;
+        if (typeof value === 'function') return `${ChatColor.FUNCTION_COLOR}f()${ChatColor.RESET}`;
+
+        if (Array.isArray(value)) {
+            if (knownElements.has(value)) {
+                return `${ChatColor.CYCLE_COLOR}[...cycle...]${ChatColor.RESET}`;
+            }
+            if (value.length === 0) {
+                return `${ChatColor.ARRAY_BRACKETS_COLOR}[]${ChatColor.RESET}`;
+            }
+            let result = `${ChatColor.ARRAY_BRACKETS_COLOR}[${ChatColor.RESET}\n`;
+            knownElements.add(value);
+            value.forEach((item, index) => {
+                result += `${indentSpace}  ${ChatColor.prettyChatJSONInternal(item, indentLevel + 1, knownElements)}`;
+                result += (index < value.length - 1 ? `,\n` : '\n');
+            });
+            knownElements.delete(value);
+            return result + `${indentSpace}${ChatColor.ARRAY_BRACKETS_COLOR}]${ChatColor.RESET}`;
+        }
+
+        if (typeof value === 'object') {
+            if (knownElements.has(value)) {
+                return `${ChatColor.CYCLE_COLOR}[...cycle...]${ChatColor.RESET}`;
+            }
+            let keySet:Set<string> = new Set();
+            let prototype = Object.getPrototypeOf(value);
+            let keys = Object.keys(prototype);
+            while (keys.length > 0) {
+                keys.forEach(key => keySet.add(key));
+                prototype = Object.getPrototypeOf(prototype);
+                keys = Object.keys(prototype);
+            }
+            Object.keys(value).forEach(key => keySet.add(key));
+            const allKeys = [...keySet].sort();
+            const entries = allKeys.map((key: string) => [key, value[key]]).filter(([key, val]) => typeof val !== 'function' && val !== void 0);
+
+            if (entries.length === 0) {
+                return `${ChatColor.OBJECT_BRACKETS_COLOR}{}${ChatColor.RESET}`;
+            }
+            let result = `${ChatColor.OBJECT_BRACKETS_COLOR}{${ChatColor.RESET}\n`;
+            let compactResult = `${ChatColor.OBJECT_BRACKETS_COLOR}{${ChatColor.RESET}`;
+
+            knownElements.add(value);
+            entries.forEach(([key, val], index) => {
+                let compactVal = this.prettyChatJSONInternal(val, indentLevel + 1, knownElements);
+                result += `${indentSpace}  ${this.KEY_COLOR}${key}${this.RESET}: ${compactVal}`;
+                result += (index < entries.length - 1) ? `,\n` : '\n';
+                compactResult += `${this.KEY_COLOR}${key}${this.RESET}: ${compactVal}`;
+                compactResult += (index < entries.length - 1) ? `, ` : '';
+            });
+            knownElements.delete(value);
+            result += `${indentSpace}${ChatColor.OBJECT_BRACKETS_COLOR}}${ChatColor.RESET}`;
+            compactResult += `${ChatColor.OBJECT_BRACKETS_COLOR}}${ChatColor.RESET}`;
+            if (compactResult.length < ChatColor.COMPACT_THRESHOLD) {
+                return compactResult;
+            }
+            return result;
+        }
+
+        return `${ChatColor.RESET}${value}`;
+    }
+
+    private static escapeString(str: string): string {
+        return str.replace(/\\/g, ChatColor.ESCAPE_COLOR + '\\\\' + ChatColor.STRING_COLOR)
+            .replace(/"/g, ChatColor.ESCAPE_COLOR + '\\"' + ChatColor.STRING_COLOR)
+            .replace(/\n/g, ChatColor.ESCAPE_COLOR + '\\n' + ChatColor.STRING_COLOR)
+            .replace(/\r/g, ChatColor.ESCAPE_COLOR + '\\r' + ChatColor.STRING_COLOR)
+            .replace(/\t/g, ChatColor.ESCAPE_COLOR + '\\t' + ChatColor.STRING_COLOR);
+    }
+}
+
---
+++ src/logging.ts
@@ -0,0 +1,280 @@
+import { system, world } from "@minecraft/server"
+import ChatColor from "./chatColor";
+
+/**
+ * The `LogLevel` class defines the various logging levels used by the logger.
+ */
+export class LogLevel {
+  static All: LogLevel = new LogLevel(2, 'all');
+  static Trace: LogLevel = new LogLevel(2, 'trace', ChatColor.DARK_AQUA);
+  static Debug: LogLevel = new LogLevel(1, 'debug', ChatColor.AQUA);
+  static Info: LogLevel = new LogLevel(0, 'info', ChatColor.GREEN);
+  static Warn: LogLevel = new LogLevel(-1, 'warn', ChatColor.GOLD);
+  static Error: LogLevel = new LogLevel(-2, 'error', ChatColor.RED);
+  static Fatal: LogLevel = new LogLevel(-3, 'fatal', ChatColor.DARK_RED);
+  static Off: LogLevel = new LogLevel(-100, 'off');
+
+  /**
+   * The list of all available log levels.
+   */
+  static values = [
+    LogLevel.All,
+    LogLevel.Trace,
+    LogLevel.Debug,
+    LogLevel.Info,
+    LogLevel.Warn,
+    LogLevel.Error,
+    LogLevel.Fatal,
+    LogLevel.Off
+  ]
+
+  /**
+   * The constructor for each log level.
+   *
+   * @param {number} level - The numerical level for this logger.
+   * @param {string} name - The string name for this logger.
+   * @param {ChatColor} color - The color to use for this logger. Defaults to `ChatColor.RESET`.
+   */
+  private constructor(public level: number, public name: string, public color: ChatColor = ChatColor.RESET) {}
+
+  /**
+   * Return the logging level as a string.
+   *
+   * @returns {string} The string representation of the logging level.
+   */
+  public toString(): string {
+    return this.color + this.name.toUpperCase() + ChatColor.RESET;
+  }
+
+  /**
+   * Parse a string to get the corresponding `LogLevel`.
+   *
+   * @param {string} str - The string to parse.
+   * @returns {LogLevel} The corresponding `LogLevel`, or `undefined` if none was found.
+   */
+  static parse(str: string): LogLevel|undefined {
+    str = str.toLowerCase();
+    for (const level of LogLevel.values) {
+      if (level.name === str) return level;
+    }
+    // check if it is a number
+    const num = parseInt(str);
+    if (!isNaN(num)) {
+      for (const level of LogLevel.values) {
+        if (level.level === num) return level;
+      }
+    }
+    return undefined;
+  }
+}
+
+/**
+* Function to match the provided string to the given pattern.
+*
+* @param {string} pattern - The pattern to match.
+* @param {string} str - The string to match the pattern against.
+* @returns {boolean} return true if the pattern matches, else returns false.
+*/
+function starMatch(pattern: string, str: string): boolean {
+  if (pattern === '*') return true
+  if (pattern.includes('*')) {
+    if (pattern.startsWith('*')) {
+      return str.endsWith(pattern.substring(1))
+    }
+    if (pattern.endsWith('*')) {
+      return str.startsWith(pattern.substring(0, pattern.length - 1))
+    }
+    const regex = new RegExp(pattern.replace(/\*/g, '.*'))
+    return regex.test(str)
+  }
+  return pattern === str
+}
+
+const loggingSettings: {
+  filter: string[];
+  level: LogLevel;
+  formatFunction: (level: LogLevel, logger: Logger, message: string) => string
+} = {
+  level: LogLevel.Info,
+  filter: ['*'],
+  formatFunction: (level: LogLevel, logger: Logger, message: string) => {
+    return `[${level}][${ChatColor.MATERIAL_EMERALD}${logger.name}${ChatColor.RESET}] ${message}`
+  }
+}
+
+/**
+* The Logger class.
+*/
+export class Logger {
+  /**
+  *  Initialize logger class
+  */
+  static init() {
+    system.afterEvents.scriptEventReceive.subscribe((ev) => {
+      if (ev.id === 'logging:level') {
+        if (!ev.message) {
+          loggingSettings.level = LogLevel.Info;
+          world.sendMessage(`${ChatColor.AQUA}Logging level set to ${loggingSettings.level}`);
+        } else {
+          const level = LogLevel.parse(ev.message);
+          if (level) {
+            loggingSettings.level = level;
+            world.sendMessage(`${ChatColor.AQUA}Logging level set to ${loggingSettings.level}`);
+          } else {
+            world.sendMessage(`${ChatColor.DARK_RED}Invalid logging level ${ev.message}`);
+          }
+        }
+      } else if (ev.id === 'logging:filter') {
+        if (!ev.message) {
+          loggingSettings.filter = ['*'];
+        } else {
+          loggingSettings.filter = ev.message.split(',');
+        }
+      }
+    });
+  }
+  /**
+  * @param {LogLevel} level - The level to set.
+  */
+  static setLevel(level: LogLevel) {
+    loggingSettings.level = level;
+  }
+  /**
+  * Filter the loggers by the given tags. Tags can use the `*` wildcard.
+  * @param {array} filter - The filter to set.
+  */
+  static setFilter(filter: string[]) {
+    loggingSettings.filter = filter;
+  }
+  /**
+  * Set the format function for the logger.
+  * @param {function} func - The function to set.
+  */
+  static setFormatFunction(func: (level: LogLevel, logger: Logger, message: string) => string) {
+    loggingSettings.formatFunction = func;
+  }
+  /**
+  * Returns a new Logger.
+  *
+  * @param {string} name - The name of the Logger.
+  * @param {array} tags - The tags for the Logger as strings.
+  *
+  * @returns {Logger} A new Logger.
+  */
+  static getLogger(name: string, ...tags: string[]): Logger {
+    return new Logger(name, tags);
+  }
+  /**
+  * Construct a new Logger
+  *
+  * @param {string} name - The name of the Logger.
+  * @param {array} tags - The tags for the logger as strings.
+  */
+  private constructor(public name: string, public tags: string[] = []) {
+  }
+
+  /**
+  * Log messages with the level set.
+  *
+  * @param {LogLevel} level - The LogLevel to log the messages at.
+  * @param {array} message - An array of the messages to log.
+  */
+  private log(level: LogLevel, ...message: any[]) {
+    if (level.level < loggingSettings.level.level) return;
+    if (loggingSettings.filter.length === 0 || this.tags.length === 0) {
+      this.logRaw(level, ...message);
+      return;
+    }
+    for (const filter of loggingSettings.filter) {
+      if (starMatch(filter, this.name)) {
+        this.logRaw(level, ...message);
+        return;
+      }
+    }
+  }
+
+  /**
+  * Internal function to log messages with the level set, that bypasses the filters.
+  *
+  * @param {LogLevel} level - The LogLevel to log the messages at.
+  * @param {array} message - An array of the messages to log.
+  */
+  private logRaw(level: LogLevel, ...message: any[]) {
+    const msg:string = message.map((x:any) => {
+      if (x === void 0) {
+        return ChatColor.GOLD + 'undefined';
+      }
+      if (x === null) {
+        return ChatColor.GOLD + 'null';
+      }
+      if (x && x.stack && x.message) {
+        return `${ChatColor.DARK_RED}${ChatColor.BOLD}${x.message}\n${ChatColor.RESET}${ChatColor.GRAY}${ChatColor.ITALIC}${x.stack}`;
+      }
+      if (typeof x === 'object' || Array.isArray(x)) {
+        return ChatColor.prettyChatJSON(x);
+      }
+      return x;
+    }).join(ChatColor.RESET + ', ');
+    const formatted = loggingSettings.formatFunction(level, this, msg);
+    world.sendMessage(formatted);
+    if ((console as any).originalLog) {
+      (console as any).originalLog(ChatColor.stripColor(formatted));
+    } else {
+      console.log(ChatColor.stripColor(formatted));
+    }
+  }
+
+  /**
+   * Logs a trace message.
+   *
+   * @param {...any} message - The message(s) to be logged.
+   */
+  trace(...message: any[]) {
+    this.log(LogLevel.Trace, ...message);
+  }
+
+  /**
+   * Logs debug message.
+   *
+   * @param {...any[]} message - The message(s) to be logged.
+   */
+  debug(...message: any[]) {
+    this.log(LogLevel.Debug, ...message);
+  }
+
+  /**
+   * Logs an informational message.
+   *
+   * @param {...any[]} message - The message(s) to be logged.
+   */
+  info(...message: any[]) {
+    this.log(LogLevel.Info, ...message);
+  }
+
+  /**
+   * Logs a warning message.
+   *
+   * @param {...any[]} message - The warning message or messages to be logged.
+   */
+  warn(...message: any[]) {
+    this.log(LogLevel.Warn, ...message);
+  }
+
+  /**
+   * Logs an error message.
+   *
+   * @param {...any[]} message - The error message(s) to log.
+   */
+  error(...message: any[]) {
+    this.log(LogLevel.Error, ...message);
+  }
+
+  /**
+   * Logs a fatal error.
+   *
+   * @param {any[]} message - The error message to log.
+   */
+  fatal(...message: any[]) {
+    this.log(LogLevel.Fatal, ...message);
+  }
+}
