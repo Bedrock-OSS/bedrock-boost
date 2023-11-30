7017b23ae0a01967884a77097fc24fefd7b05d5d
stirante <brzozowski.s.piotr@gmail.com>
2023-11-30 12:06:17 +0100

    Add documentation

--- src/polyfill/polyfill.ts
+++ src/polyfill/polyfill.ts
@@ -1,8 +1,13 @@
 import { world } from "@minecraft/server";
-import { install as playerInstall } from "../playerPolyfill";
+import { install as playerInstall } from "./playerPolyfill";
 
 export default class Polyfill {
   private static _installed: Map<string, boolean> = new Map<string, boolean>();
+  /**
+   * Installs the polyfill for the console.
+   * 
+   * This polyfill makes `console.log` send a message to the world chat.
+   */
   public static installConsole(): void {
     if (this._installed.get('console')) return;
     this._installed.set('console', true);
@@ -13,6 +18,13 @@ export default class Polyfill {
       world.sendMessage(message);
     };
   }
+  /**
+   * Installs the polyfill for the player.
+   * 
+   * This polyfill adds the following methods to the player:
+   * - applyImpulse
+   * - clearVelocity
+   */
   public static installPlayer(): void {
     if (this._installed.get('player')) return;
     this._installed.set('player', true);
