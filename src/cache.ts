48d8ebcc7444383db38782dbc3a263584d5e2319
stirante <brzozowski.s.piotr@gmail.com>
2024-01-11 14:08:27 +0100

    Add caching dimension height range

--- src/cache.ts
+++ src/cache.ts
@@ -1,6 +1,9 @@
+import { NumberRange } from "@minecraft/common";
 import { Dimension, world } from "@minecraft/server";
 
 const dimensions: { [key: string]: Dimension } = {};
+
+const dimensionHeightRanges: { [key: string]: NumberRange } = {};
 
 /**
  * Returns a dimension from the cache or gets it from the API and caches it.
@@ -10,4 +13,14 @@ const dimensions: { [key: string]: Dimension } = {};
 export function getDimension(name: string) {
   if (dimensions[name]) return dimensions[name];
   return dimensions[name] = world.getDimension(name);
+}
+
+/**
+ * Returns a dimension height range from the cache or gets it from the API and caches it.
+ * @param name The name of the dimension.
+ * @returns The dimension height range.
+ */
+export function getDimensionHeightRange(name: string) {
+  if (dimensionHeightRanges[name]) return dimensionHeightRanges[name];
+  return dimensionHeightRanges[name] = getDimension(name).heightRange;
 }
