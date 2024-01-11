b69df713e8f685ea90ae45bd9a9f43406d59cb78
stirante <brzozowski.s.piotr@gmail.com>
2024-01-11 14:09:05 +0100

    Export new functions

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import Vec3 from "./vec3";
 import Timings from "./timings";
 import { addIdleDummy, clearIdleDummy } from "./profilingUtils";
-import { getDimension } from "./cache";
+import { getDimension, getDimensionHeightRange } from "./cache";
 import { Logger, LogLevel } from "./logging";
 import ChatColor from "./chatColor";
 import ColorJSON from "./colorJson";
 import Polyfill from "./polyfill/polyfill";
+import { jobProgressPromise, jobPromise } from "./jobUtils";
+import { isValidLocation } from "./blockUtils";
 
 
-export { Vec3, Timings, addIdleDummy, clearIdleDummy, getDimension, LogLevel, Logger, ChatColor, Polyfill, ColorJSON };
+export {
+  Vec3, 
+  Timings, 
+  addIdleDummy, 
+  clearIdleDummy, 
+  getDimension, 
+  getDimensionHeightRange, 
+  LogLevel, 
+  Logger, 
+  ChatColor, 
+  Polyfill, 
+  ColorJSON,
+  jobPromise,
+  jobProgressPromise,
+  isValidLocation
+};
