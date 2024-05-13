9b1fae8d89a635ad1c4741876205d69e4b64790d
stirante <brzozowski.s.piotr@gmail.com>
2024-05-13 12:26:25 +0200

    Add cache for block permutations

--- src/Cache.ts
+++ src/Cache.ts
@@ -1,9 +1,11 @@
 import { NumberRange } from "@minecraft/common";
-import { Dimension, world } from "@minecraft/server";
+import { BlockPermutation, Dimension, world } from "@minecraft/server";
 
 const dimensions: { [key: string]: Dimension } = {};
 
 const dimensionHeightRanges: { [key: string]: NumberRange } = {};
+
+const permutations: { [key: string]: BlockPermutation } = {};
 
 /**
  * Returns a dimension from the cache or gets it from the API and caches it.
@@ -23,4 +25,16 @@ export function getDimension(name: string) {
 export function getDimensionHeightRange(name: string) {
   if (dimensionHeightRanges[name]) return dimensionHeightRanges[name];
   return dimensionHeightRanges[name] = getDimension(name).heightRange;
+}
+
+/**
+ * Returns a block permutation from the cache or gets it from the API and caches it.
+ * @param blockName The name of the block.
+ * @param states The block states.
+ * @returns The block permutation.
+ */
+export function getBlockPermutation(blockName: string, states?: Record<string, boolean | number | string>): BlockPermutation {
+  const key = `${blockName}:${JSON.stringify(states)}`;
+  if (permutations[key]) return permutations[key];
+  return permutations[key] = BlockPermutation.resolve(blockName, states);
 }
