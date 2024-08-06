e09f926cf4e7b2325cf47dbd82dd7eb05e5f7d27
stirante <brzozowski.s.piotr@gmail.com>
2024-08-06 10:50:02 +0200

    Add TimeOfDay values

---
+++ src/TimeOfDay.ts
@@ -0,0 +1,8 @@
+export default class TimeOfDay {
+  public static readonly Day = 1000;
+  public static readonly Midnight = 18000; 
+  public static readonly Night = 13000;
+  public static readonly Noon = 6000;
+  public static readonly Sunrise = 23000;
+  public static readonly Sunset = 12000;
+}
