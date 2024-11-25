40b1c78fd6d91e34884025f110c07f1712ad1b85
stirante <brzozowski.s.piotr@gmail.com>
2024-11-25 11:23:23 +0100

    Add sendMolangData

---
+++ src/VariableSender.ts
@@ -0,0 +1,40 @@
+import { Entity, Player } from "@minecraft/server";
+
+/**
+ * Represents a Molang expression. This will be injected into the variable value as Molang expression.
+ */
+export type MolangExpression = {
+  /**
+   * The Molang expression to inject.
+   */
+  value: string
+};
+
+/**
+ * Represents a Molang value. This can be a number, boolean, or Molang expression.
+ */
+export type MolangValue = number | boolean | MolangExpression;
+
+/**
+ * Sends Molang variables to an entity using `playanimation` command.
+ * @param entity The entity to send the data to.
+ * @param animation The RP animation to send the data through.
+ * @param data The data to send.
+ * @param receivers Players to send the data to. If not specified, the data will be sent to all players.
+ */
+export function sendMolangData(entity: Entity, animation: string, data: { [key: string]: MolangValue }, receivers: Player[] = []) {
+  const stopExpression = Object.entries(data).map(([key, value]) => {
+    if (typeof value === "number") {
+      return `${key}=${value}`;
+    } else if (typeof value === "boolean") {
+      return `${key}=${value ? 1 : 0}`;
+    } else {
+      return `${key}=${value.value}`;
+    }
+  }).join(";") + ";return 0;";
+  entity.playAnimation(animation, {
+    stopExpression: stopExpression,
+    controller: "__" + animation + "_send_data__",
+    players: receivers.length ? receivers.map(player => player.name) : undefined
+  });
+}
