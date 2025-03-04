3781658830f03a5e29da1ccd728aba42db4230fd
stirante <brzozowski.s.piotr@gmail.com>
2025-03-04 09:30:05 +0100

    Add isItem command utility

--- src/utils/CommandUtils.ts
+++ src/utils/CommandUtils.ts
@@ -1,13 +1,58 @@
-import { Dimension, InputPermissionCategory, Player, Vector3 } from "@minecraft/server";
+import {
+  Dimension,
+  Entity,
+  InputPermissionCategory,
+  Player,
+  Vector3,
+} from "@minecraft/server";
 
 export enum InputPermission {
   Movement = "movement",
-  Camera = "camera"
+  Camera = "camera",
 }
 export enum CameraShakeType {
   Positional = "positional",
   Rotational = "rotational",
 }
+export enum SlotLocation {
+  Armor = "slot.armor",
+  Body = "slot.armor.body",
+  ArmorChest = "slot.armor.chest",
+  Feet = "slot.armor.feet",
+  Head = "slot.armor.head",
+  Legs = "slot.armor.legs",
+  Chest = "slot.chest",
+  EnderChest = "slot.enderchest",
+  Equippable = "slot.equippable",
+  Hotbar = "slot.hotbar",
+  Inventory = "slot.inventory",
+  Saddle = "slot.saddle",
+  MainHand = "slot.weapon.mainhand",
+  OffHand = "slot.weapon.offhand",
+}
+
+export type Quantity =
+  | number
+  | {
+      min?: number;
+      max: number;
+    }
+  | {
+      min: number;
+      max?: number;
+    };
+
+export type ItemMatcher = {
+  // these properties are always allowed
+  item?: string;
+  quantity?: Quantity;
+  data?: number;
+  slotLocation?: SlotLocation;
+  slot?: number;
+} & // if slot is specified, slotLocation must also be defined
+(| { slot?: undefined; slotLocation?: SlotLocation }
+  | { slot: number; slotLocation: SlotLocation }
+);
 
 /**
  * This is a collection of utility methods for working with commands until a proper API is available.
@@ -19,13 +64,18 @@ export default class CommandUtils {
    * @param player The player for whom to set the input permission.
    * @param permission The input permission to set.
    * @param value The value to set the input permission to.
-   * 
+   *
    * @deprecated Use `player.inputPermissions.setPermissionCategory` instead.
    */
-  public static setInputPermission(player: Player, permission: InputPermission, value: boolean): void {
+  public static setInputPermission(
+    player: Player,
+    permission: InputPermission,
+    value: boolean
+  ): void {
     player.inputPermissions.setPermissionCategory(
-      permission === InputPermission.Movement ?
-        InputPermissionCategory.Movement : InputPermissionCategory.Camera,
+      permission === InputPermission.Movement
+        ? InputPermissionCategory.Movement
+        : InputPermissionCategory.Camera,
       value
     );
   }
@@ -36,8 +86,17 @@ export default class CommandUtils {
    * @param intensity - The intensity of the camera shake effect.
    * @param duration - The duration of the camera shake effect in seconds.
    */
-  public static addCameraShake(player: Player, type: CameraShakeType, intensity: number, duration: number): void {
-    player.runCommand(`camerashake add @s ${intensity.toFixed(20)} ${duration.toFixed(20)} ${type}`);
+  public static addCameraShake(
+    player: Player,
+    type: CameraShakeType,
+    intensity: number,
+    duration: number
+  ): void {
+    player.runCommand(
+      `camerashake add @s ${intensity.toFixed(20)} ${duration.toFixed(
+        20
+      )} ${type}`
+    );
   }
   /**
    * Stops the camera shake for the specified player.
@@ -52,6 +111,51 @@ export default class CommandUtils {
    * @param location The location of the block to destroy.
    */
   public static destroyBlock(dimension: Dimension, location: Vector3): void {
-    dimension.runCommand(`setblock ${location.x} ${location.y} ${location.z} air destroy`);
+    dimension.runCommand(
+      `setblock ${location.x} ${location.y} ${location.z} air destroy`
+    );
+  }
+  /**
+   * Checks if an entity have items matching the specified matchers.
+   * @param entity The entity to check.
+   * @param matchers The matchers to check.
+   * @returns True if the entity has items matching the matchers, false otherwise.
+   * 
+   * @remarks
+   * This method uses the `testfor` command and `hasitem` selector, so that you can check for the data of the item.
+   */
+  public static isItem(entity: Entity, matchers: ItemMatcher[]): boolean {
+    let cmd = `testfor @s[hasitem=[`;
+    for (let i = 0; i < matchers.length; i++) {
+      const matcher = matchers[i];
+      cmd += `{item=${matcher.item}`;
+      if (matcher.quantity !== void 0) {
+        cmd += ",quantity=";
+        if (typeof matcher.quantity === "number") {
+          cmd += matcher.quantity;
+        } else {
+          if (matcher.quantity.min != void 0) {
+            cmd += matcher.quantity.min;
+          }
+          cmd += "..";
+          if (matcher.quantity.max != void 0) {
+            cmd += matcher.quantity.max;
+          }
+        }
+      }
+      if (matcher.data !== void 0) {
+        cmd += `,data=${matcher.data}`;
+      }
+      if (matcher.slotLocation !== void 0) {
+        cmd += `,${matcher.slotLocation}=${matcher.slot}`;
+        if (matcher.slot !== void 0) {
+          cmd += `,slot=${matcher.slot}`;
+        }
+      }
+      cmd += "}";
+    }
+    cmd += ']]';
+    const result = entity.runCommand(cmd);
+    return result.successCount > 0;
   }
 }
