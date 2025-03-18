e4d079358c1393dce34d05c578c43a4b630b4362
stirante <brzozowski.s.piotr@gmail.com>
2025-03-18 11:58:27 +0100

    Update ItemUtils to be a static class

--- src/utils/ItemUtils.ts
+++ src/utils/ItemUtils.ts
@@ -1,7 +1,98 @@
-import { EntityEquippableComponent, EquipmentSlot, ItemDurabilityComponent, Player } from "@minecraft/server";
+import { EntityEquippableComponent, EquipmentSlot, ItemDurabilityComponent, ItemEnchantableComponent, Player } from "@minecraft/server";
 import { Logger } from "../Logging";
 
 const log = Logger.getLogger("itemUtils", "bedrock-boost", "itemUtils");
+
+export interface ConsumeDurabilityOptions {
+  /**
+   * Whether to ignore enchantments when consuming durability.
+   */
+  ignoreEnchantments?: boolean;
+  /**
+   * The amount of durability to consume. Defaults to 1.
+   */
+  value?: number;
+  /**
+   * The slot to consume durability from. Defaults to the player's main hand.
+   */
+  slot?: EquipmentSlot;
+  /**
+   * Whether to suppress playing a sound when item breaks.
+   */
+  suppressSound?: boolean;
+}
+
+export default class ItemUtils {
+  /**
+   * Consumes durability from the player's selected item.
+   * @param player - The player whose item durability will be consumed.
+   * @param value - The amount of durability to consume. Defaults to 1.
+   * @param slot - The slot to consume durability from. Defaults to the player's selected slot.
+   * @returns True if the durability was consumed, false otherwise.
+   * 
+   * @remarks 
+   * Return value `false` does not always mean that the function failed. It can also mean that the item was not damaged due to unbreaking enchantment.
+   */
+  public static consumeDurability(player: Player, options: ConsumeDurabilityOptions = {}): boolean {
+    if (options.value === void 0) {
+      options.value = 1;
+    }
+    if (options.slot === void 0) {
+      options.slot = EquipmentSlot.Mainhand;
+    }
+    if (options.value < 1 || !player) {
+      log.error("Invalid value or player");
+      return false;
+    }
+    const equippable = player.getComponent(EntityEquippableComponent.componentId) as EntityEquippableComponent;
+    if (!equippable) {
+      log.error("Player equippable component not found");
+      return false;
+    }
+    const item = equippable.getEquipment(options.slot);
+    if (!item) {
+      log.debug("No item in selected slot");
+      return false;
+    }
+    const durabilityComponent = item.getComponent(ItemDurabilityComponent.componentId) as ItemDurabilityComponent;
+    if (!durabilityComponent) {
+      log.error("Item has no durability component");
+      return false;
+    }
+    if (!options.ignoreEnchantments) {
+      const enchantable = item.getComponent(ItemEnchantableComponent.componentId) as ItemEnchantableComponent;
+      if (enchantable) {
+        const unbreakingLevel = enchantable.getEnchantment('unbreaking')?.level ?? 0;
+        if (ItemUtils.getUnbreakingChance(unbreakingLevel) < Math.random()) {
+          return false;
+        }
+      }
+    }
+    if (durabilityComponent.damage + options.value >= durabilityComponent.maxDurability) {
+      log.trace("Item is broken");
+      equippable.setEquipment(options.slot, undefined);
+      if (!options.suppressSound) {
+        player.playSound('random.break');
+      }
+      return true;
+    }
+    durabilityComponent.damage = durabilityComponent.damage + options.value;
+    log.trace(`Item durability is now ${durabilityComponent.damage}/${durabilityComponent.maxDurability}`);
+    equippable.setEquipment(options.slot, item);
+    return true;
+  }
+  /**
+   * Returns the chance to consume durability from item based on the unbreaking level.
+   * @param unbreakingLevel - The unbreaking level of the item.
+   * @returns The chance to consume durability from item based on the unbreaking level.
+   */
+  public static getUnbreakingChance(unbreakingLevel: number) {
+    if (unbreakingLevel === 0) {
+      return 1;
+    }
+    return 1 / (unbreakingLevel + 1);
+  }
+}
 
 /**
  * Consumes durability from the player's selected item.
@@ -9,34 +100,14 @@ const log = Logger.getLogger("itemUtils", "bedrock-boost", "itemUtils");
  * @param value - The amount of durability to consume. Defaults to 1.
  * @param slot - The slot to consume durability from. Defaults to the player's selected slot.
  * @returns True if the durability was successfully consumed, false otherwise.
+ * 
+ * @deprecated Use `ItemUtils.consumeDurability` instead.
  */
 export function consumeDurability(player: Player, value: number = 1, slot: EquipmentSlot = EquipmentSlot.Mainhand): boolean {
-  if (value < 1 || !player) {
-    log.error("Invalid value or player");
-    return false;
-  }
-  const equippable = player.getComponent(EntityEquippableComponent.componentId) as EntityEquippableComponent;
-  if (!equippable) {
-    log.error("Player equippable component not found");
-    return false;
-  }
-  const item = equippable.getEquipment(slot);
-  if (!item) {
-    log.debug("No item in selected slot");
-    return false;
-  }
-  const durabilityComponent = item.getComponent(ItemDurabilityComponent.componentId) as ItemDurabilityComponent;
-  if (!durabilityComponent) {
-    log.error("Item has no durability component");
-    return false;
-  }
-  if (durabilityComponent.damage + value >= durabilityComponent.maxDurability) {
-    log.trace("Item is broken");
-    equippable.setEquipment(slot, undefined);
-    return true;
-  }
-  durabilityComponent.damage = durabilityComponent.damage + value;
-  log.trace(`Item durability is now ${durabilityComponent.damage}/${durabilityComponent.maxDurability}`);
-  equippable.setEquipment(slot, item);
-  return true;
+  return ItemUtils.consumeDurability(player, {
+    slot,
+    value,
+    ignoreEnchantments: true,
+    suppressSound: true,
+  });
 }
